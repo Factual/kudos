278acfff9d74cdf94c04e12460517967ca826b1a
Byron Lutz <byron@factual.com>
2016-06-07 16:55:52 -0700

    Make sure only internal users can log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
+    if user
+      session[:user_id] = user.id
+    else
+      flash[:error] = "Doesn't look like you're an internal user. Try logging in with another account."
+    end
     redirect_to root_path
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
+    return false unless has_internal_email(auth.info.email)
      where(auth.slice(:provider, :uid).to_hash).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
@@ -9,5 +10,14 @@ class User < ApplicationRecord
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
+  end
+
+  private
+
+  def self.has_internal_email(email)
+    Settings.email_domains.each do |domain|
+      return true if email.ends_with? "@#{domain}"
+    end
+    false
   end
 end
