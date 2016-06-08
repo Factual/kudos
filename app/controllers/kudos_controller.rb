bdd09ea9d4dfd9d5cc2582efb0e4ad075a44ed60
Aaron Crow <dirtyvagabond@yahoo.com>
2016-06-07 19:39:50 -0700

    First working kudo create

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -3,7 +3,34 @@ class KudosController < ApplicationController
     render json: Kudo.all
   end
 
+  # Creates a new Kudo based on current user and specified receiver & body
+  #
+  # Required params:
+  #   :receiver_email [email of receiver]
+  #   :body           [body of kudo]
   def create
-    render plain: params[:kudo].inspect
+    giver_id = current_user.id
+    receiver_email = params[:kudo][:receiver_email]
+    receiver = User.find_by(email: receiver_email)
+
+    kudo = Kudo.new({giver_id: giver_id, 
+                     receiver_id: receiver.id, 
+                     body: params[:kudo][:body]})
+
+    if kudo.save
+      Rails.logger.debug("**** SAVED! kudo id = #{kudo.id}  8-]")
+    else
+      Rails.logger.debug("**** looked up receiver: #{recevier}")
+    end
+
+    #TODO: REST/post resp?
+    render json: params[:kudo].inspect
   end
+
+  private
+  def kudo_params
+    params.require(:kudo).permit(:body)
+  end
+
+
 end
