5ef9f973dd0a7af3989eb488f9cab4974e72061c
Byron Lutz <byron@factual.com>
2016-06-07 18:23:25 -0700

    Use default omniauth configuration to enforce factual.com

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    return false unless is_internal_account?(auth)
     where(auth.slice(:provider, :uid).to_hash).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
@@ -11,14 +10,5 @@ class User < ApplicationRecord
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
-  end
-
-  private
-
-  def self.is_internal_account?(auth)
-    Settings.google_apps_domains.each do |domain|
-      return true if domain == auth.extra.id_info.hd
-    end
-    false
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Settings.google_oauth.client_id, Settings.google_oauth.client_secret
+  provider :google_oauth2,
+    Settings.google_oauth.client_id,
+    Settings.google_oauth.client_secret,
+    {
+      access_type: 'online',
+      hd: Settings.google_apps_domains
+    }
 end
