a288c4a3fa33570261eb3e43d0ed028ea3ffc862
Byron Lutz <byron@factual.com>
2016-06-07 17:06:01 -0700

    Added user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
     return false unless has_internal_email(auth.info.email)
-     where(auth.slice(:provider, :uid).to_hash).first_or_initialize.tap do |user|
+    where(auth.slice(:provider, :uid).to_hash).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.email = auth.info.email
+      user.avatar = auth.info.image
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
@@ -15,6 +16,7 @@ class User < ApplicationRecord
   private
 
   def self.has_internal_email(email)
+    # TODO: change this to use hd
     Settings.email_domains.each do |domain|
       return true if email.ends_with? "@#{domain}"
     end
