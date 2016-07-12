92b32755d58902c3bead8ef0cbd1099c435452f0
Byron Lutz <byron@factual.com>
2016-07-12 16:56:17 -0700

    Changed kudos index endpoint to include user info

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,6 +1,6 @@
 class KudosController < ApplicationController
   def index
-    render json: Kudo.all
+    render json: expand_user_info(Kudo.all)
   end
 
   # Creates a new Kudo based on current user and specified receiver & body
@@ -30,6 +30,29 @@ class KudosController < ApplicationController
   end
 
   private
+
+  def expand_user_info(kudos)
+    kudos_array = []
+    kudos.each do |kudo|
+      kudo_hash = kudo.attributes
+      kudo_hash['giver'] = basic_user_info_by_id kudo.giver_id
+      kudo_hash.delete 'giver_id'
+      kudo_hash['receiver'] = basic_user_info_by_id kudo.receiver_id
+      kudo_hash.delete 'receiver_id'
+      kudos_array.push kudo_hash
+    end
+    kudos_array
+  end
+
+  def basic_user_info_by_id(id)
+    user = User.find(id)
+    {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    }
+  end
 
   def kudo_params
     params.require(:kudo).permit(:body, :receiver_email)
