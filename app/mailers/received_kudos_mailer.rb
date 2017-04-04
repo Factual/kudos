e09b479f81cfad964ab1becbb6c2e223bb956c8b
William Chang <willchang@gmail.com>
2017-04-04 13:56:55 -0700

    Edits to Email Notifications v1 #10 pull request

--- app/mailers/received_kudos_mailer.rb
+++ app/mailers/received_kudos_mailer.rb
@@ -1,13 +1,10 @@
 class ReceivedKudosMailer < ApplicationMailer
 
-  # Generated using `docker-compose run web rails generate mailer ReceivedKudosMailer`
-  # NOTE: associated views need to be created in ../views/received_kudos_mailer/
-
   def received_kudos_notification(kudo)
     @kudo = kudo
     @giver = User.find_by(id: @kudo.giver_id)
     @receiver = User.find_by(id: @kudo.receiver_id)
-    @url = 'http://kudos.factual.com'
+    @url = ENV["EMAIL_SITE_URL"]
     mail(to: @receiver.email, subject: "You just received Kudos from #{@giver.name}!")
   end
 
