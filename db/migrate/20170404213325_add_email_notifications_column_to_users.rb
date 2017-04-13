f2e4fa9d9b23abeb129c60cb436a2b27a8c62418
William Chang <willchang@gmail.com>
2017-04-13 11:31:22 -0700

    adding not null constraint to email notifications column of users table

--- db/migrate/20170404213325_add_email_notifications_column_to_users.rb
+++ db/migrate/20170404213325_add_email_notifications_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddEmailNotificationsColumnToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :email_notifications, :boolean, :default => true
+    add_column :users, :email_notifications, :boolean, :default => true, :null => false
   end
 end
