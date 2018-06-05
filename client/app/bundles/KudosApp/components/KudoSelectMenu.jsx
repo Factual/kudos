f1fc65763cb05532614936ded5b51d1a625f5254
Arpan Pal <arpan@Arpans-MacBook-Pro.local>
2018-06-05 15:25:12 -0700

    multi-selection fully functional

--- client/app/bundles/KudosApp/components/KudoSelectMenu.jsx
+++ client/app/bundles/KudosApp/components/KudoSelectMenu.jsx
@@ -2,16 +2,16 @@ import React, { PropTypes } from 'react';
 import Select from 'react-select';
 
 function constructOptions(key, index, returnArray, map) {
-  const filterValue = `${key} ${map[key]}`;
+  const filterValue = `${key} | ${map[key]}`;
   const displayValue = `${key} (${map[key]})`;
   returnArray.push({value: filterValue, label: displayValue})
 }
-// display={displayValue}
 export default class KudoSelectMenu extends React.Component {
 
   static propTypes = {
     allUsers: PropTypes.object.isRequired,
     onChange: PropTypes.func.isRequired,
+    userSuggestions: PropTypes.array.isRequired,
   };
 
   render() {
@@ -23,12 +23,11 @@ export default class KudoSelectMenu extends React.Component {
     return (<Select closeOnSelect={false}
                     disabled={false}
                     placeholder="Select a person!"
-                    multi
+                    multi={true}
+                    simpleValue={true}
+                    value={this.props.userSuggestions}
                     options={children}
-                    removeSelected={true}
                     rtl={false}
-                    simpleValue
                     onChange={this.props.onChange}/>)
   }
 }
-
