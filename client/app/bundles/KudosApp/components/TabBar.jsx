f01c3e4d67e1e6e1ccbddbf125b1e63f4475ad87
Arpan Pal <arpan@factual.com>
2018-06-20 15:16:04 -0700

    converted redux to mobx

--- client/app/bundles/KudosApp/components/TabBar.jsx
+++ client/app/bundles/KudosApp/components/TabBar.jsx
@@ -2,25 +2,27 @@ import React, { PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import AppStore from '../stores/AppStore'
 
-const DropdownItem = ({ itemName, currentTab}) => {
-  const classNameForItem = 'nav-link' + (currentTab == itemName ? ' active' : '');
+const DropDownItem = ({ itemName, currentTab}) => {
+  const classNameForItem = 'nav-link' + (currentTab === itemName ? ' active' : '');
 
   return <option className={`${classNameForItem} +  kudos-list__tab`} value={itemName}>
     {itemName}
   </option>
-}
+};
 
-const TabBar = ({ currentTab, setActiveTab }) => {
-  const drop_down = ['All Kudos', 'My Kudos', 'Awarded Kudos']
-  function itemChanger(event) {
-    setActiveTab(event.target.value)
+@observer
+export class TabBar extends React.Component {
+  static itemChanger(event) {
+    AppStore.kudosStore.setCurrentTab(event.target.value)
   }
-  return (
+  render() {
+    const drop_down = ['All Kudos', 'My Kudos', 'Awarded Kudos']
+    return (
     <div className="kudo_list-changer kudos-list">
-    <select onChange={itemChanger} className={"drop_down_menu"}>
-      {drop_down.map(drop_down => <DropdownItem key={drop_down}
+    <select onChange={TabBar.itemChanger} className={"drop_down_menu"}>
+      {drop_down.map(drop_down => <DropDownItem key={drop_down}
                                                 itemName={drop_down}
-                                                currentTab={currentTab}/>)}</select>
+                                                currentTab={AppStore.kudosStore.currentTab}/>)}</select>
       <svg width="100%" height="10">
         <line x1="0" x2="100%" y1="5" y2="5"
               stroke="#898989"
@@ -29,57 +31,7 @@ const TabBar = ({ currentTab, setActiveTab }) => {
               strokeDasharray=".5,5"
               />
       </svg>
-    </div>
-
-@observer
-export class TabBar extends React.Component {
-  render() {
-    const tabs = ['Recent', 'My Kudos', 'Awarded Kudos']
-
-    return (
-      <ul className="kudos-list__nav">
-        {tabs.map(tab => (
-          <Tab
-            key={tab}
-            tabName={tab}
-            currentTab={AppStore.kudosStore.currentTab}
-            clickHandler={() => AppStore.kudosStore.setCurrentTab(tab)}
-          />
-        ))}
-      </ul>
-    )
-  }
-}
-  return <option className={`${classNameForItem} +  kudos-list__tab`} value={itemName}>
-    {itemName}
-  </option>
-}
-
-const TabBar = ({ currentTab, setActiveTab }) => {
-  const drop_down = ['All Kudos', 'My Kudos', 'Awarded Kudos']
-  function itemChanger(event) {
-    setActiveTab(event.target.value)
-  }
-  return (
-    <div>
-    <select onChange={itemChanger} className={"drop_down_menu"}>
-      {drop_down.map(drop_down => <DropdownItem key={drop_down}
-                                                itemName={drop_down}
-                                                currentTab={currentTab}/>)}</select>
-      <p className="dotted_line"/>
-    </div>
-
-  );
-};
-
-const Tab = ({ tabName, currentTab, clickHandler }) => {
-  const classNameForTab = 'nav-link' + (currentTab == tabName ? ' active' : '')
-
-  return (
-    <li className="kudos-list__tab" onClick={clickHandler}>
-      <span className={classNameForTab}>{tabName}</span>
-    </li>
-  )
+    </div>)};
 }
 
 export default TabBar
