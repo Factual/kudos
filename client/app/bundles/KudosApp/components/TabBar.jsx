daa2e15e14d9ff7ada42795d364a13293f86d8bf
Arpan Pal <arpan@factual.com>
2018-06-27 13:48:54 -0700

    style cleanups

--- client/app/bundles/KudosApp/components/TabBar.jsx
+++ client/app/bundles/KudosApp/components/TabBar.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import AppStore from '../stores/AppStore'
 
-const DropDownItem = ({ itemName, currentTab}) => {
+const DropDownItem = ({itemName, currentTab}) => {
   const classNameForItem = 'nav-link' + (currentTab === itemName ? ' active' : '');
 
   return <option className={`${classNameForItem} +  kudos-list__tab`} value={itemName}>
@@ -15,24 +15,26 @@ export class TabBar extends React.Component {
   static itemChanger(event) {
     AppStore.kudosStore.setCurrentTab(event.target.value)
   }
+
   render() {
     const drop_down = ['All Kudos', 'Kudos Received', 'Kudos Sent']
     const gray = "#898989";
     return (
-    <div className="kudo_list-changer kudos-list">
-    <select onChange={TabBar.itemChanger} className="drop_down_menu">
-      {drop_down.map(drop_down => <DropDownItem key={drop_down}
-                                                itemName={drop_down}
-                                                currentTab={AppStore.kudosStore.currentTab}/>)}</select>
-      <svg width="100%" height="10">
-        <line x1="0" x2="100%" y1="5" y2="5"
-              stroke={gray}
-              strokeWidth="2.5"
-              strokeLinecap="square"
-              strokeDasharray=".5,5"
-              />
-      </svg>
-    </div>)};
+      <div className="kudo_list-changer kudos-list">
+        <select onChange={TabBar.itemChanger} className="drop_down_menu">
+          {drop_down.map(drop_down => <DropDownItem key={drop_down}
+                                                    itemName={drop_down}
+                                                    currentTab={AppStore.kudosStore.currentTab}/>)}</select>
+        <svg width="100%" height="10">
+          <line x1="0" x2="100%" y1="5" y2="5"
+                stroke={gray}
+                strokeWidth="2.5"
+                strokeLinecap="square"
+                strokeDasharray=".5,5"
+          />
+        </svg>
+      </div>)
+  };
 }
 
 export default TabBar
